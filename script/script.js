fe630dd9231c712ccb5dcbe30ec22886263431f9
RUTUKAPATE <101728762+RUTUKAPATE@users.noreply.github.com>
2023-09-14 02:24:52 +0530

    Done

--- script/script.js
+++ script/script.js
@@ -10,3 +10,13 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
